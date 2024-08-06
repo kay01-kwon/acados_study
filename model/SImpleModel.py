c0f3ba3b4640132510347b48558435c1312a9bb0
kay <kay1216@yonsei.ac.kr>
2024-08-06 20:20:12 +0900

    MHE model for test

--- model/SImpleModel.py
+++ model/SImpleModel.py
@@ -11,14 +11,40 @@ class SImpleModel:
 
         self.model = AcadosModel()
 
-        self.m = 2;
+        self.m = 2
 
         self.p = cs.MX.sym('p',1)
         self.v = cs.MX.sym('v',1)
         self.x = cs.vertcat(self.p, self.v)
 
+        self.u = cs.MX.sym('u')
+
         self.dpdt = cs.MX.sym('dpdt',1)
         self.dvdt = cs.MX.sym('dvdt',1)
         self.xdot = cs.vertcat(self.dpdt, self.dvdt)
 
-        
+
+    def get_acados_model(self):
+
+        self.f_expl = cs.vertcat(self.p_kinematics(), self.v_dynamics)
+        self.f_impl = cs.vertcat(self.dpdt, self.dvdt)
+
+        self.model.f_expl_expr = self.f_expl
+        self.model.f_impl_expr = self.f_impl
+
+        self.model.x = self.x
+        self.model.u = self.u
+        self.model.xdot = self.xdot
+
+        self.model.model_name = self.model_name
+
+        return self.model
+
+
+    def p_kinematics(self):
+
+        return self.v
+
+    def v_dynamics(self):
+
+        return self.u/self.m
