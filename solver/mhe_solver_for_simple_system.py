89a35d8b572b7d1f9d639419a1e3891d2a9d3d10
kay <kay1216@yonsei.ac.kr>
2024-08-06 22:55:35 +0900

    MHE solver completed

--- solver/mhe_solver_for_simple_system.py
+++ solver/mhe_solver_for_simple_system.py
@@ -5,7 +5,7 @@ from model.mhe_simple_model import MheSimpleModel
 import casadi as cs
 from scipy.linalg
 class MheSolverForSimpleSystem:
-    def __init__(self, N, Tf, Q, Q0, R):
+    def __init__(self, N, Tf, Q, R, R0):
         '''
         :param N: Number of shooting nodes
         :param Tf: prediction horizon
@@ -18,6 +18,8 @@ class MheSolverForSimpleSystem:
 
         self.ocp_mhe.model = MheSimpleModel().get_acados_model()
 
+        self.Tf = Tf
+
         # Dimension info
         self.nx_augmented = self.ocp_mhe.model.x.rows()
         self.nparam = self.ocp_mhe.model.p.rows()
@@ -25,33 +27,71 @@ class MheSolverForSimpleSystem:
         self.ny = Q.shape[0] + R.shape[0]                   # state, noise
         self.nu = self.ocp_mhe.model.u.rows()
         self.ny_e = 0
-        self.ny_0 = Q.shape[0] + R.shape[0] + Q0.shape[0]   # state, noise, Arrival cost
+        self.ny_0 = Q.shape[0] + R.shape[0] + R0.shape[0]   # state, noise, Arrival cost
 
         # Set the number of shooting nodes
 
         self.ocp_mhe.dims.N = N
 
-        self.ocp_mhe.cost.cost_type = 'LINEAR_LS'
-        self.ocp_mhe.cost.cost_type_e = 'LINEAR_LS'
+        self.ocp_mhe.cost.cost_type = 'LINEAR_LS'       # Linear least square
+        self.ocp_mhe.cost.cost_type_e = 'LINEAR_LS'     # Linear least square
+        self.ocp_mhe.cost.cost_type_0 = 'LINEAR_LS'     # Linear least square
 
-        # Setup weight for cost
+        self.ocp_mhe.parameter_values = np.zeros((self.nparam,))
 
-        # 1. Vx weight
-        self.ocp_mhe.cost.Vx = np.zeros((self.ny, self.nx))
-        self.ocp_mhe.cost.Vx[:self.nx, :self.nx] = np.eye(self.nx)
+        self.set_ocp_cost()
 
-        # 2. Vx arrival weight
-        self.ocp_mhe.cost.Vx_e = np.zeros((self.nx,))
-        self.ocp_mhe.cost.Vx_e = np.eye(self.nx,)
+        self.set_ocp_solver()
 
-        # 3. Vu weight
-        self.ocp_mhe.cost.Vu = np.zeros((self.ny, self.nu))
-        self.ocp_mhe.cost.Vu[-self.nu:, -self.nu:] = np.eye(self.nu)
+        self.acados_mhe_solver = AcadosOcpSolver(self.ocp_mhe)
 
-        # 4. Weight for state cost
-        self.ocp_mhe.cost.W = scipy.linalg.block_diag(Q, R)
+        self.acados_mhe_solver.cost_set(0, "W", scipy.linalg.block_diag(Q, R, R0))
 
-        
+
+        def set_ocp_cost(self):
+            # Setup weight for cost
+
+            # 1. Vx weight (Lagrangian term)
+            self.ocp_mhe.cost.Vx = np.zeros((self.ny, self.nx))
+            self.ocp_mhe.cost.Vx[:self.nx, :self.nx] = np.eye(self.nx)
+
+            # 2. Vx (Mayer term)
+            self.ocp_mhe.cost.Vx_e = np.zeros((self.nx,))
+            self.ocp_mhe.cost.Vx_e = np.eye(self.nx, )
+
+            # 3. Vu weight
+            self.ocp_mhe.cost.Vu = np.zeros((self.ny, self.nu))
+            self.ocp_mhe.cost.Vu[-self.nu:, -self.nu:] = np.eye(self.nu)
+
+            # 4. Weight for state cost
+            self.ocp_mhe.cost.W_0 = scipy.linalg.block_diag(Q, R, R0)
+            self.ocp_mhe.cost.W = scipy.linalg.block_diag(Q, R)
+
+            self.ocp_mhe.cost.yref = np.zeros((self.ny,))
+            self.ocp_mhe.cost.yref_e = np.zeros((self.ny_e,))
+            self.ocp_mhe.cost.yref_0 = np.zeros((self.ny_0,))
+
+        def set_ocp_solver(self):
+
+            # Set QP solver
+            self.ocp_mhe.solver.options.qp_solver = 'FULL_CONDENSING_QPOASES'
+            self.ocp_mhe.solver.options.hessian_approx = 'GAUSS_NEWTON'
+            self.ocp_mhe.solver.options.integrator_type = 'ERK'
+
+            self.ocp_mhe.solver.options.nlp_solver_type = 'SQP'
+            self.ocp_mhe.solver.options.nlp_solver_max_iter = 200
+
+            # Set prediction horizon
+            self.ocp_mhe.solver.options.tf = self.Tf
+
+        def get_ocp_solver(self):
+
+            return self.acados_mhe_solver
+
+
+
+
+
 
 
 
